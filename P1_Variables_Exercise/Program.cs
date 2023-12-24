ffcf8fd0d8f717e65f462ba209b1da4bc23e8af8
PradipJathar <jatharpradipofficial@gmail.com>
2023-12-24 20:50:17 +0530

    Variable Exercise

---
+++ P1_Variables_Exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_Variables_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Addition of numbers
+
+            int num1 = 25;
+            int num2 = 30;
+            int sum;
+
+            sum = num1 + num2;
+
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
