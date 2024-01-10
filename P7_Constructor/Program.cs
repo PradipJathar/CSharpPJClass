7a68e0322673c638a136298a6cb6519fcdcfea5f
PradipJathar <jatharpradipofficial@gmail.com>
2024-01-10 16:39:25 +0530

    Constructor

---
+++ P7_Constructor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P7_Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee pradip = new Employee("Pradip Jathar", "Thermax", 252045.60);
+        }
+    }
+
+
+    class Employee
+    {
+        public string name;
+        public string company;
+        public double salery;
+        
+        public Employee(string empName, string empCompany, double empSalery)
+        {
+            name = empName;
+            company = empCompany;
+            salery = empSalery;
+        }
+    }
+}
